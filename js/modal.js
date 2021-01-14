8b092c37f1f034553c4ec5224461face2779cac0
William Wallace <william.wallace@wcc.govt.nz>
2021-01-14 16:05:23 +1300

    Add buggy lightbox

--- js/modal.js
+++ js/modal.js
@@ -1,9 +1,9 @@
 // Get the modal
-var modal = document.getElementById("myModal");
+var modal = document.getElementById("myModal2");
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById("myImg");
-var modalImg = document.getElementById("img01");
+var img = document.getElementById("myImg2");
+var modalImg = document.getElementById("img02");
 var captionText = document.getElementById("caption");
 img.onclick = function(){
   modal.style.display = "block";
